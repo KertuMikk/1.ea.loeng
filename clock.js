45e63ad37ee493465cf74dc3587cb0c74d7db001
Kertu Mikk <kertumik@tlu.ee>
2018-02-09 13:49:52 +0200

    kella lisad

--- clock.js
+++ clock.js
@@ -1,23 +1,50 @@
-//avalikud muutujad 
+// Avalikud muutujad
 let clockContainer // = null
-
+ 
 
 
 window.onload = function () {
-init()
+  init()
 }
-function init(){
-	const clockContainer = document.querySelector('#clock')
-	console.log(clockContainer)
-	startClock()
 
-}
+function init () {
+  clockContainer = document.querySelector('#clock')
+  console.log(clockContainer)
  
- function startClock (){
- 	
-	 window.setInterval(function() {
-	 const date = new Date()
-	 clockContainer.innerHTML = date
-	 }
-	 ,1000)
- }
+
+  startClock()
+
+  //document.querySelector('#size-up').onclick
+  document.querySelector('#size-down').addEventListener('click',sizeDown) //2 erinevat versiooni kuidas kuulata
+  document.querySelector('#size-up').addEventListener('click',sizeUp)
+}
+function sizeUp(){
+	console.log('+')
+	clockContainer.style.fontSize='25px'
+	window.addEventListener('keypress', hideClock)
+	
+}
+function sizeDown(){
+	clockContainer.style.fontSize='5px'
+	console.log('-')
+	window.addEventListener('keypress', hideClock)
+}
+function hideClock(event){
+	console.log(event)
+	if(event.key =='h')
+	clockContainer.style.display = 'none'
+	//clockContainer.style.visibility = 'hidden'
+}
+function startClock () {
+  updateClock()
+
+  window.setInterval(function () {
+   updateClock()
+  }, 1000)
+}
+
+function updateClock () {
+ 
+
+  clockContainer.innerHTML = new Date()
+}
